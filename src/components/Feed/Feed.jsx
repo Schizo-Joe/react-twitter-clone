169e8f9326e42fba79b713d73661c80b179cc2f7
korathbasil <korathbasil@live.com>
2020-08-08 22:13:53 +0530

    post component finished

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feed.css';
 import TweetBox from '../TweetBox/TweetBox';
 import Post from '../Post/Post';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Feed() {
     return (
@@ -10,12 +11,38 @@ function Feed() {
                 <h2>Home</h2>
             </div>
             <TweetBox />
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
-            <Post imageUrl="https://source.unsplash.com/random/300x200"/>
+            <Post 
+                avatar={AccountCircleIcon}
+                displayName="Lionel Messi"
+                userName="leomessi"
+                verified={true}
+                postContent="We wiil win today, may GOD save the KINg"
+                postMedia="https://source.unsplash.com/random/300x200"
+                />
+                <Post 
+                avatar={AccountCircleIcon}
+                displayName="Lionel Messi"
+                userName="leomessi"
+                verified={true}
+                postContent="We wiil win today, may GOD save the KINg"
+                postMedia="https://source.unsplash.com/random/300x200"
+                />
+                <Post 
+                avatar={AccountCircleIcon}
+                displayName="Lionel Messi"
+                userName="leomessi"
+                verified={true}
+                postContent="We wiil win today, may GOD save the KINg"
+                postMedia="https://source.unsplash.com/random/300x200"
+                />
+                <Post 
+                avatar={AccountCircleIcon}
+                displayName="Lionel Messi"
+                userName="leomessi"
+                verified={true}
+                postContent="We wiil win today, may GOD save the KINg"
+                postMedia="https://source.unsplash.com/random/300x200"
+                />
         </div>
     )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Post.css';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 
 function Post(props) {
     return (
-        <div>
-           <img src={props.imageUrl} alt=""/> 
+        <div className="post">
+           <props.avatar className="post__avatar" style={{ fontSize: 55}} />
+           <div className="post__contents">
+               <div className="post__contents__names">
+                    <p class="account__name">{props.displayName}</p>
+                    <VerifiedUserIcon className="verified__icon" style={{display:(props.verified)?"inline-block":"none"}} />
+                    <p class="user__name">@{props.userName}</p>
+                </div>
+               <div className="post__contents__text">
+                <p>{props.postContent}</p>
+               </div>
+               <div className="post__content__media">
+                <img src={props.postMedia} alt=""/>
+               </div>
+               <div className="post__contents__reactions">
+
+               </div>
+           </div>
         </div>
     )
 }
